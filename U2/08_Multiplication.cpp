98c938328bc8eb6c9dafb6fae3521e5a5b01a68c
UP210739 <(up210739@alumnos.upa.edu.mx)>
2022-10-12 10:28:13 -0500

    01

---
+++ U2/08_Multiplication.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float number, multiple, result, counter, table;
+
+    cout << "Type the number to multiply: ";
+    cin >> number;
+    cout << "Type how many times to multiply: ";
+    cin >> multiple;
+    cout << endl;
+
+    for (float counter = 1;counter <= multiple; counter++)
+    {
+        result = number*counter;
+        cout << "|";
+        
+        for (float table = 0; table < 47; table++)
+        {
+            cout << "-";
+        }
+        cout << "|";
+        cout << "\n";
+        cout << "|" << "\t" << number << "\t" << "*" << "\t" << counter << "\t" << "=" << "\t" << result << "\t" << "|" << endl;
+    }
+
+    return 0;
+}
