3e8eefa88c56d03e49022156860af6f7da20a6f0
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-11-23 10:22:47 -0600

    conect

--- U4/Connect4.cpp
+++ U4/Connect4.cpp
@@ -1,2 +1,10 @@
 #define FILAS 6
 #define COLUMNAS 7
+#define TAMAÑO_MATRIZ sizeof(char)FILAS*COLUMNAS
+#define JUGADOR_1 'O'
+#define JUGADOR_2 'X
+#define JUGADOR_CPU_1 JUGADOR_1
+#define JUGADOR_CPU_2 JUGADOR_2
+#define ESPACIO_VACIO ' '
+#define FILA_NO_ENCONTRADA -1
+
