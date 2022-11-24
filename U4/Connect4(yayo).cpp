cc2b58f046081f20219f22a67d4502d183e11a4e
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-11-23 19:44:20 -0600

    conect

--- U4/Connect4(yayo).cpp
+++ U4/Connect4(yayo).cpp
@@ -1,4 +1,4 @@
-#define FILAS 6
+/*#define FILAS 6
 #define COLUMNAS 7
 #define TAMAÑO_MATRIZ sizeof(char)FILAS*COLUMNAS
 #define JUGADOR_1 'O'
@@ -6,5 +6,31 @@
 #define JUGADOR_CPU_1 JUGADOR_1
 #define JUGADOR_CPU_2 JUGADOR_2
 #define ESPACIO_VACIO ' '
-#define FILA_NO_ENCONTRADA -1
+#define FILA_NO_ENCONTRADA -1 */
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+void dibujarEncabezado (int columnas){
+    cout << endl;
+    int i;
+    for ( i = 0; i < columnas; ++i) {
+        cout << "|%d", i +1;
+        if (i+1 >= columnas)
+        {
+            cout<< "|";
+        }
+        
+    }
+}
+
+int main(){
+
+    
+}
+
+
 
