f3229c5591420665d46fe56c819acd403a51aeb5
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-11-30 12:22:40 -0600

    conecta

--- U4/Connect4(yayo).cpp
+++ U4/Connect4(yayo).cpp
@@ -1,66 +1,380 @@
-/*#define FILAS 6
-#define COLUMNAS 7
-#define TAMAÑO_MATRIZ sizeof(char)FILAS*COLUMNAS
-#define JUGADOR_1 'O'
-#define JUGADOR_2 'X
-#define JUGADOR_CPU_1 JUGADOR_1
-#define JUGADOR_CPU_2 JUGADOR_2
-#define ESPACIO_VACIO ' '
-#define FILA_NO_ENCONTRADA -1 */
+/*
+Date:
+Author: César William Muro Hernández
+Email: up210739@alumnos.upa.edu.mx
+Description:
+Last Modification:
+*/
 
 #include <iostream>
+#include <time.h>
+
 using namespace std;
-#include <stdio.h>
 
-int obtenerFilaDesocupada(int columna, char tablero[FILAS][COLUMNAS]){
-    int i;
-    for (i = FILAS -1; i >= 0 ,i--){
-        if (tablero[i][columna]= espacio_Vacio)
-        {
-            return i;
-        }
-    return fila_No_Encontrada;
-    }
-    
-}
+void game(char board[3][3]);
+void enterMove(char board[3][3]);
+void boardBody(char board[3][3]);
+void entryMove(char board[3][3]);
+void computer (char board[3][3]);
+int winner (char board[3][3]);
 
-void dibujarEncabezado (int columnas){
-    cout << endl;
-    int i;
-    for ( i = 0; i < columnas; ++i) {
-        cout << "|%d", i +1;
-        if (i+1 >= columnas)
-        {
-            cout<< "|";
-        }
-        
-    }
-}
+void game(char board[3][3])
+{
+    int counter1=0, counter2;
 
+    enterMove(board);
 
-int dibujarTablero(char tablero[FILAS][COLUMNAS]){
-    dibujarEncabezado(COLUMNAS);
-    cout << endl;
-    int i;
-    for ( i = 0; i < FILAS; ++i)
+    do
     {
-        int j;
-        for (j = 0; j < COLUMNAS; ++j)
+        system ("clear");
+        boardBody(board);
+
+        if (counter1 % 2 == 0)
         {
-            cout << "|%c",tablero[i][j];
-            if (j+1 >=COLUMNAS)
-            {
-                cout << "|";
-            }
-            cout << endl;
+            entryMove(board);
         }
+        else
+        {
+            computer(board);
+        }
+
+        counter2 = winner(board);
         
+        counter1++;
+        
+    } while (counter1 <= 9 && counter2 == 3);
+
+    system ("clear");
+    boardBody(board);
+
+    if (counter2 < 2)
+    {
+        cout << endl << ",◦⁺˚*･༓☾ Win ☽༓･*˚⁺◦,"<< endl << endl;
     }
-    return 0;
+    else if (counter2 > 1)
+    {
+        cout << endl << ",◦⁺˚*･༓☾ Lose ☽༓･*˚⁺◦,"<< endl << endl;
+    }
+    else
+    {
+        cout << endl << ",◦⁺˚*･༓☾ None win ☽༓･*˚⁺◦,"<< endl << endl;
+    }
+    
+    
     
 }
 
+void enterMove(char board[3][3])
+{
+    int counter1, counter2;
+    char entry;
 
+    entry = '1';
 
-int main(){
+    for (int counter1 = 0; counter1 < 3; counter1++)
+    {
+        for (int counter2 = 0; counter2 < 3; counter2++)
+        {
+            board[counter1][counter2] = entry++;
+        }
+    }
+}
+
+void entryMove(char board[3][3])
+{
+    int counter1, counter2, counter3;
+    char enter;
+
+    do
+    {
+        do
+        {
+            cout << endl << "Enter a movement: ";
+            cin >> board[3][3];
+            enter = board[3][3];
+        } while (enter < '1' || enter > '9');
+
+        counter3 = 0;
+
+        switch (enter)
+        {
+        case '1':
+        {
+            counter1 = 0;
+            counter2 = 0;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '2':
+        {
+            counter1 = 0;
+            counter2 = 1;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '3':
+        {
+            counter1 = 0;
+            counter2 = 2;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '4':
+        {
+            counter1 = 1;
+            counter2 = 0;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '5':
+        {
+            counter1 = 1;
+            counter2 = 1;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '6':
+        {
+            counter1 = 1;
+            counter2 = 2;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '7':
+        {
+            counter1 = 2;
+            counter2 = 0;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '8':
+        {
+            counter1 = 2;
+            counter2 = 1;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        case '9':
+        {
+            counter1 = 2;
+            counter2 = 2;
+            if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+            {
+                counter3 = 1;
+                cout << "Invalid movement!";
+            }
+            break;
+        }
+        }
+    } while (counter3 == 1);
+
+    board[counter1][counter2] = 'X';
+}
+
+void computer (char board[3][3]){
+    int counter1, counter2, number;
+    srand (time(NULL));
+
+    do
+    {
+        counter1 = rand() % 3;
+        counter2 = rand() % 3;
+        number = 0;
+        if (board[counter1][counter2] == 'X' || board[counter1][counter2] == 'O')
+        {
+            number = 1;
+        }
+        
+    } while (number == 1);
+
+    board[counter1][counter2] = 'O';
+}
+
+void boardBody(char board[3][3])
+{
+    int counter1, counter2;
+
+    cout << endl << ",◦⁺˚*･༓☾ Cat game ☽༓･*˚⁺◦," << endl << endl;
+
+    for (int counter1 = 0; counter1 < 3; counter1++)
+    {
+        cout << "        |     |     " << endl;
+        for (int counter2 = 0; counter2 < 3; counter2++)
+        {
+            if (counter2 == 0)
+            {
+                cout << "     " << board[counter1][counter2] << "  |";
+            }
+
+            else if (counter2 == 1)
+            {
+                cout << "  " << board[counter1][counter2] << "  |";
+            }
+            else
+            {
+                cout << "  " << board[counter1][counter2] << "  ";
+            }
+        }
+
+        if (counter1 < 2)
+        {
+            cout << endl
+                 << "   _____|_____|_____" << endl;
+        }
+        else
+        {
+            cout << endl
+                 << "        |     |     "<< endl;
+        }
+    }
+}
+
+int winner (char board[3][3]){
+    if (board[0][0] == 'X' || board[0][0] == 'O')
+    {
+        if (board[0][0] == board[0][1] && board[0][0] == board[0][2])
+        {
+            if (board[0][0] == 'X')
+            {
+                return 1;
+            }
+            else if (board[0][0] == 'O')
+            {
+                return 2;
+            }
+            
+        }
+        else if (board[0][0] == board[1][0] && board[0][0] == board[2][0])
+        {
+            if (board[0][0] == 'X')
+            {
+                return 1;
+            }
+            else if (board[0][0] == 'O')
+            {
+                return 2;
+            }
+        }
+
+    }
+    else if (board[1][1] == 'X' || board[1][1] == 'O')
+    {
+        if (board[1][1] == board[0][1] && board[1][1] == board[2][2])
+        {
+            if (board[1][1] == 'X')
+            {
+                return 1;
+            }
+            else if (board[1][1] == 'O')
+            {
+                return 2;
+            }
+            
+        }
+        else if (board[1][1] == board[0][1] && board[1][1] == board[2][1])
+        {
+            if (board[1][1] == 'X')
+            {
+                return 1;
+            }
+            else if (board[1][1] == 'O')
+            {
+                return 2;
+            }
+        }
+        else if (board[1][1] == board[1][0] && board[1][1] == board[1][2])
+        {
+            if (board[1][1] == 'X')
+            {
+                return 1;
+            }
+            else if (board[1][1] == 'O')
+            {
+                return 2;
+            }
+        }
+        else if (board[1][1] == board[0][2] && board[1][1] == board[2][0])
+        {
+            if (board[1][1] == 'X')
+            {
+                return 1;
+            }
+            else if (board[1][1] == 'O')
+            {
+                return 2;
+            }
+        }
+        
+    }
+    else if (board[2][2] == 'X' || board[2][2] == 'O')
+    {
+        if (board[2][2] == board[2][0] && board[2][2] == board[2][1])
+        {
+            if (board[2][2] == 'X')
+            {
+                return 1;
+            }
+            else if (board[2][2] == 'O')
+            {
+                return 2;
+            }
+            
+        }
+        else if (board[2][2] == board[0][2] && board[2][2] == board[1][2])
+        {
+            if (board[2][2] == 'X')
+            {
+                return 1;
+            }
+            else if (board[2][2] == 'O')
+            {
+                return 2;
+            }
+        }
+
+    }
+
+    return 3;
+    
+}
+
+int main()
+{
+    char board[3][3];
+
+    game(board);
+
+    return 0;
 }
